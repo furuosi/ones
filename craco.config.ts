e40312a44ef281146a3a08d111bb8afc133f1e5c
LinHanLove <2188817393@qq.com>
2023-06-01 17:40:13 +0800

    fix(components):

--- craco.config.ts
+++ craco.config.ts
@@ -1,21 +1,8 @@
-module.exports = {
+export default {
   devSever: {
     proxy: {}
   },
   webpack: {
-    module: {
-      rules: [
-        {
-          test: /\.svg$/i,
-          type: 'asset',
-          resourceQuery: '.src/logo.svg' // *.svg?url
-        },
-        {
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: [{ loader: '@svgr/webpack', options: { icon: true } }]
-        }
-      ]
-    }
+    module: {}
   }
 }

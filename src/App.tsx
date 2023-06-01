ffc3b67db3ec6682ee969c949dc3a7cf63ffbaa8
LinHanLove <2188817393@qq.com>
2023-06-01 17:41:28 +0800

    feat(utils): daasjnfj
    fafaf f
    
    BREAKING CHANGE:
    effwe
    
    ISSUES CLOSED: #31

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React88888
         </a>
       </header>
     </div>
